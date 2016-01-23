dcffdd0cfebeb639d366e781328ec2f51b4583eb
niyang <niyang@sunboxsoft.com>
2016-01-23 21:28:53 +0800

    'mod'

--- core/src/main/java/com/kld/gsm/ATG/domain/MonitorInventory.java
+++ core/src/main/java/com/kld/gsm/ATG/domain/MonitorInventory.java
@@ -33,6 +33,15 @@ public class MonitorInventory extends MonitorInventoryKey {
 
     private String oilno;
 
+    private String version;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
 
     public double getOilcubage() {
         return oilcubage;
