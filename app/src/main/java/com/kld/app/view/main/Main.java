a067b4f199ebe0b55fd876490532dd095eddfefc
yinzhiguang <yinzhiguang1@163.com>
2016-01-25 00:28:17 +0800

    'update'

--- app/src/main/java/com/kld/app/view/main/Main.java
+++ app/src/main/java/com/kld/app/view/main/Main.java
@@ -145,6 +145,7 @@ import java.util.List;
 
     private void initialize() {
         frame = new JFrame();
+        frame.setTitle("液位仪应用系统");
         frame.setBounds(100, 100, 800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(null);
