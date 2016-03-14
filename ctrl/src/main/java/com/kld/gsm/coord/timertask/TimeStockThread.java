f51b3fb48eb9641b6214989b4d312752d7cf50af
yinzhiguang <yinzhiguang1@163.com>
2016-03-14 20:26:28 +0800

    'update'

--- ctrl/src/main/java/com/kld/gsm/coord/timertask/TimeStockThread.java
+++ ctrl/src/main/java/com/kld/gsm/coord/timertask/TimeStockThread.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Component;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 
@@ -38,16 +40,19 @@ public class TimeStockThread extends Thread {
                     int minute = Integer.parseInt(sd.format(date));
                     int second = Integer.parseInt(sd2.format(date));
                     logger.info("首次进入读取当前分，休眠直到下一个整点开始保存数据。以后每小时休眠minute:"+minute);
-                    sleep(((60 - minute) * 60 - second) * 1000);
+                    sleep(getHour(minute, second));
                     flag = false;
                 }else{
                     Date date_minute = new Date();
                     int minute_difference = Integer.parseInt(sd.format(date_minute));
                     int second_difference = Integer.parseInt(sd2.format(date_minute));
-                    sleep(((60-minute_difference) * 60 - second_difference) * 1000);
+                    sleep(getHour(minute_difference, second_difference));
                 }
             } catch (InterruptedException e) {
                 logger.error("整点库存异常:"+e);
+                e.printStackTrace();
+            }catch (ParseException e){
+                logger.error("整点库存异常ParseException:"+e);
                 e.printStackTrace();
             }
             try {
@@ -61,5 +66,29 @@ public class TimeStockThread extends Thread {
                 e.printStackTrace();
             }
         }
+    }
+
+//    public static void main(String[] args)throws Exception{
+//        //18:19:11  长度是1罐号是0   18点左右10分钟调用一次实时库存
+//        System.out.println(getHour(1,0));
+//    }
+
+    public static long getHour(int minute,int second) throws ParseException {
+        Calendar currentTime = Calendar.getInstance();
+        String d = "2016-03-31 23:59:59";
+        SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date ddd = sd.parse(d);
+        currentTime.setTime(ddd);
+        int currentHour = currentTime.get(currentTime.HOUR);
+        currentTime.set(Calendar.HOUR_OF_DAY, currentHour + 1);
+        currentTime.set(Calendar.MINUTE, 0);
+        currentTime.set(Calendar.SECOND, 0);
+        currentTime.set(Calendar.MILLISECOND, 0);
+        Date NextHour = currentTime.getTime();
+        currentTime.set(Calendar.HOUR_OF_DAY, currentHour);
+        currentTime.set(Calendar.MINUTE, minute);
+        currentTime.set(Calendar.MILLISECOND, second);
+        Date Hour = currentTime.getTime();
+        return (NextHour.getTime() - Hour.getTime());
     }
 }
